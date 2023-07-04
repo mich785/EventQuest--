2402ca9934d1a5d45a84454024ed030fe00ea7a0
michelle nderi <nderimichelle@gmail.com>
2023-07-04 22:08:44 +0300

    updated index method for reviewscontroller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,11 +2,25 @@ class ReviewsController < ApplicationController
     def index
         reviews = Review.all 
         if session[:user_id].present?
-            render json: reviews, include: :event
+            render json: reviews ,status: :ok
         else
-            render json: { errors: ["Unauthorized"] }, status: :unauthorized
+            render json: { errors: "Unauthorized"}, status: :unauthorized
         end
     end
 
-    
+    def create
+        if session[:user_id].present?
+          review = review.new(review_params)
+          review.user_id = session[:user_id]
+      
+          if review.save
+            render json: review, include: { user: { only: [:username, :image_url, :bio] } }, status: :created
+          else
+            render json: { errors: review.errors.full_messages }, status: :unprocessable_entity
+          end
+        else
+          render json: { errors: "Unauthorized"}, status: :unauthorized
+        end
+      end
+      
 end
--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,4 +1,4 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id, :comment
+  attributes :id, :comment, :user_id, :event_id
   belongs_to :event
 end
