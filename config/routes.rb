a64cca7697cb0e7054162802d15ea7d2451dd310
michelle nderi <nderimichelle@gmail.com>
2023-07-04 21:30:47 +0300

    Updated users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :reviews only:[:index, :create, :destroy, :update]
+  resources :reviews, only:[:index, :create, :destroy, :update]
   resources :events
   resources :users, only:[:index,:show, :create]
+
   post '/signup', to: 'users#create'
   get '/me', to: 'users#show'
   post '/login', to: 'sessions#create'
