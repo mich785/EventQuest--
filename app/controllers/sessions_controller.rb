ea6f65101ab39e204b1b511117cedb92985b0fee
michelle nderi <nderimichelle@gmail.com>
2023-07-06 11:09:00 +0300

    Updated sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,21 @@
-def create
-    user = User.find_by(username: params[:username])
-    if user&.authenticate(params[:password])
-      session[:user_id] = user.id
-      render json: user, status: :created
-    else
-      render json: { errors: ["Invalid username or password"] }, status: :unauthorized
+class SessionsController < ApplicationController
+  def create
+      user = User.find_by(username: params[:username])
+      if user&.authenticate(params[:password])
+        session[:user_id] = user.id
+        render json: user, status: :created
+      else
+        render json: { errors: ["Invalid username or password"] }, status: :unauthorized
+      end
     end
-  end
 
-def destroy
-    if session[:user_id].present?
-    session.delete :user_id
-    head :no_content
-    else
-        render json: { errors: ["Unauthorized"]  }, status: :unauthorized
+  def destroy
+      if session[:user_id].present?
+      session.delete :user_id
+      head :no_content
+      else
+          render json: { errors: ["Unauthorized"]  }, status: :unauthorized
+      end
     end
-  end
-  
+    
+end
