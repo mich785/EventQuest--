6b46f2b63e8db4a08c1e9fb22c2707e4c957a5e6
michelle nderi <nderimichelle@gmail.com>
2023-07-07 17:12:07 +0300

    Styled the home page

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,19 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import LoginPage from "./LoginPage";
+import "../Styles/home.css";
 
-function Home() {
-  return (
-    <>
-      <h1>Welcome to EventQuest</h1>
-      <LoginPage/>
-      <div className="alternative">
-        <p>
-          Don't have an account? <Link to="/signup">Sign Up</Link>
-        </p>
+  function Home() {
+    return (
+      <div className="home-container">
+        <h1>Welcome to EventQuest</h1>
+        <LoginPage />
+        <div className="alternative">
+          <p>
+            Don't have an account? <Link to="/signup">Sign Up</Link>
+          </p>
+        </div>
       </div>
-    </>
-  );
-}
+    );
+  }
 
 export default Home;
